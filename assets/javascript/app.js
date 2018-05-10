916b4f488e5f9a45fa9bd951951946b30bc4a516
AbrahamTesla <manlangitv@gmail.com>
2018-05-09 23:20:23 -0400

    added corrections

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+var namePerson = [{question: "What's the name of Chicago Bulls Player who won championship 6 times", choices:["Bryan","Lebron", "Carter","Jordan"],
+                  Answer: 3
+                  },{
+                      question: "Who is the oracle of Omaha?",
+                      choices:"Gates", "Buffet","Musk","Tesla",
+                      Answer: 1
+                  },{
+                     question: "Who owns Microsoft?",
+                     choices: "Tesla","Gates","Bezo","Einstein",
+                     Answer: 1
+                 },{
+                     question: "Who design the modern alernating current electricity supply system?",
+                     choices: "Tesla","Gates","Bezo","Einstein",
+                     Answer: 0
+                 },{
+                     question: "Who invented the telescope",
+                     choices: "Gates","Buffet","Galileo","Einstein",
+                     Answer: 2
+                 },{
+                     question: "Who is number 24 in Lakers team?",
+                     choices: "Carter","Micheal","kobe","Einstein",
+                     Answer: 2
+                 },{
+                     question: "Who came up with the theory of relativity?",
+                     choices: "Einstein","Plato","Newton","Jobs",
+                     Answer: 0
+                 },{
+                     question: "Who is the founder of Apple?",
+                     choices: "Einstein","Plato","Socrates","Jobs",
+                     Answer: 0
+                 },{
+                     question: "Who is the founder of spaceX?",
+                     choices: "Jobs","Plato","Newton","Musk",
+                     Answer: 0
+                 },{
+                     question: "Who is the famous boxer of all time?",
+                     choices: "Pacquiao","Ali","MayWeather","Jackson",
+                     Answer: 0
+                 }];
